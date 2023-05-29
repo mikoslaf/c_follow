be1f3c80ffcc81118265438c2f2d20ded10f0e4f
mikoslaf <mikolaj2003wojcik@gmail.com>
2023-05-29 09:45:13 +0200

    add manu and event for buttons in menu / delete follow-anim command

--- Client/ClientMain.cs
+++ Client/ClientMain.cs
@@ -16,24 +16,36 @@ namespace c_follow.Client
         private Ped[] peds = new Ped[0];
         public ClientMain()
         {
-            TriggerEvent("chat:addSuggestion", "/follow", "Create npc to follow", new[] 
-            { 
-             new { name = "Model", help = "Name Model for NPC or [Random]"},
-             new { name = "Number", help = "Number of NPC [1-10]"},
-            });
+            TriggerEvent("chat:addSuggestion", "/follow", "Create npc to follow/open menu");
+
 
             API.RegisterCommand("follow", new Action<int, List<object>, string>(follow), false);
 
-            TriggerEvent("chat:addSuggestion", "/follow-anim", "Tast play anim for NPC or Reset following", new[] 
-            {
-             new { name = "dist", help = "dist"},
-             new { name = "anim", help = "anim"},
-            });
+            //TriggerEvent("chat:addSuggestion", "/follow-anim", "Tast play anim for NPC or Reset following", new[] 
+            //{
+            // new { name = "dist", help = "dist"},
+            // new { name = "anim", help = "anim"},
+            //});
 
-            API.RegisterCommand("follow-anim", new Action<int, List<object>, string>(follow_anim), false);
+            //API.RegisterCommand("follow-anim", new Action<int, List<object>, string>(follow_anim), false);
 
-            API.RegisterNuiCallbackType("spawn");
-            EventHandlers["__cfx_nui:spawn"] += new Action<ExpandoObject>(spawn);
+            API.RegisterNuiCallbackType("c_spawn");
+            EventHandlers["__cfx_nui:c_spawn"] += new Action<ExpandoObject>(spawn);
+
+            API.RegisterNuiCallbackType("c_anim");
+            EventHandlers["__cfx_nui:c_anim"] += new Action<ExpandoObject>(anim);
+
+            API.RegisterNuiCallbackType("c_delete");
+            EventHandlers["__cfx_nui:c_delete"] += new Action(delete);
+
+            API.RegisterNuiCallbackType("c_kill");
+            EventHandlers["__cfx_nui:c_kill"] += new Action(kill);
+
+            API.RegisterNuiCallbackType("c_cancel");
+            EventHandlers["__cfx_nui:c_kill"] += new Action(cancel);
+
+            API.RegisterNuiCallbackType("c_follow");
+            EventHandlers["__cfx_nui:c_follow"] += new Action(follow_again);
         }
 
         private async void spawn(dynamic data) 
@@ -64,7 +76,7 @@ namespace c_follow.Client
                     }
 
                     Ped npc = await World.CreatePed((Model)Names[rand], player.Position + (player.ForwardVector * 2));
-                    npc.Task.LookAt(player);
+                    //npc.Task.LookAt(player);
                     npc.Task.FollowToOffsetFromEntity(player, (player.ForwardVector * 2), -1, 10);
 
                     API.SetPedAsGroupMember(npc.Handle, API.GetPedGroupIndex(npc.Handle));
@@ -92,129 +104,136 @@ namespace c_follow.Client
                 }
             }
         }
-        private void follow(int source, List<object> args, string raw)
+
+        private async void anim(dynamic data) 
         {
-            API.SetNuiFocus(true, true);
-            SendNuiMessage("{\"action\":\"start\"}");
-            
-            if ((bool)args.Any())
+            API.SetNuiFocus(false, false);
+            String dist = data.dist;
+            String anim = data.anim;
+
+            while (!API.HasAnimDictLoaded(dist))
             {
-                if (peds.Length != 0)
-                {
-                    foreach (Ped i in peds)
-                    {
-                        i.Delete();
-                    }
-                }
-                byte cont = 4;
-                if (args.ElementAtOrDefault(1) != null)
-                {
-                    if (byte.TryParse(args[1].ToString(), out _))
-                    {
-                        cont = Convert.ToByte(args[1]);
-                        if (cont < 0 && cont > 40) //zmieniæ !!!!
-                        {
-                            cont = 4;
-                        }
-                    }
-                }
-
-                //Ped player = Game.Player.Character;
-                //peds = new Ped[cont];
-
-                //if (args[0].ToString() == "Random")
-                //{
-                //    String[] Names = { "a_f_m_trampbeac_01", "a_f_y_eastsa_03", "a_f_y_hipster_04", "a_m_m_genfat_01", "a_m_m_salton_02", "a_m_y_beachvesp_01", "a_m_y_clubcust_01", "a_m_y_polynesian_01" };
-                //    Random rnd = new Random();
-
-                //    for (int i = 0; i < peds.Length; i++)
-                //    {
-                //        Byte rand = (byte)rnd.Next(0, Names.Length);
-                //        uint Hash = (uint)GetHashKey(Names[rand]);
-                //        API.RequestModel(Hash);
-                //        while (!API.HasModelLoaded(Hash))
-                //        {
-                //            await BaseScript.Delay(100);
-                //        }
-
-                //        Ped npc = await World.CreatePed((Model)Names[rand], player.Position + (player.ForwardVector * 2));
-                //        npc.Task.LookAt(player);
-                //        npc.Task.FollowToOffsetFromEntity(player, (player.ForwardVector * 2), -1, 10);
-
-                //        API.SetPedAsGroupMember(npc.Handle, API.GetPedGroupIndex(npc.Handle));
-                //        API.SetPedCombatAbility(npc.Handle, 2);
-                //        peds[i] = npc;
-                //    }
-                //}
-                //else
-                //{
-                //    uint Hash = (uint)GetHashKey(args[0].ToString());
-                //    API.RequestModel(Hash);
-                //    while (!API.HasModelLoaded(Hash))
-                //    {
-                //        await BaseScript.Delay(100);
-                //    }
-                //    for (int i = 0; i < peds.Length; i++)
-                //    {
-                //        Ped npc = await World.CreatePed((Model)args[0].ToString(), player.Position + (player.ForwardVector * 2));
-                //        //npc.Task.LookAt(player);
-                //        npc.Task.FollowToOffsetFromEntity(player, (player.ForwardVector * 2), -1, 10);
-
-                //        API.SetPedAsGroupMember(npc.Handle, API.GetPedGroupIndex(npc.Handle));
-                //        API.SetPedCombatAbility(npc.Handle, 2);
-                //        peds[i] = npc;
-                //    }
-                //}
-
-            }
-            else
-            {
-                foreach (Ped i in peds)
-                {
-                    i.Delete();
-                }
-                peds = new Ped[0];
-            }
-
-        }
-
-        private async void follow_anim(int source, List<object> args, string raw)
-        {
-            String ani1 = "", ani2 = "";
-            if (args.Count >= 2)
-            {
-                ani1 = args[0].ToString();
-                ani2 = args[1].ToString();
-            }
-            if (ani1 == "" && ani2 == "")
-            {
-                Ped player = Game.Player.Character;
-                foreach (Ped i in peds)
-                {
-                    i.Task.ClearAllImmediately();
-                    i.Task.FollowToOffsetFromEntity(player, (player.ForwardVector * 2), -1, 10);
-                }
-            }
-            while (!API.HasAnimDictLoaded(ani1))
-            {
-                API.RequestAnimDict(ani1);
+                API.RequestAnimDict(dist);
                 await BaseScript.Delay(100);
             }
             AnimationFlags flags = AnimationFlags.Loop | AnimationFlags.CancelableWithMovement;
             foreach (Ped i in peds)
             {
                 i.Task.ClearAllImmediately();
-                i.Task.PlayAnimation(ani1, ani2, -1, -1, flags);
+                i.Task.PlayAnimation(dist, anim, -1, -1, flags);
             }
 
         }
 
-        //[Tick]
-        //public Task OnTick()
-        //{
-        //    DrawRect(0.5f, 0.5f, 0.5f, 0.5f, 255, 255, 255, 150);
+        private void follow_again()
+        {
+            API.SetNuiFocus(false, false);
+            Ped player = Game.Player.Character;
+            foreach (Ped i in peds)
+            {
+                i.Task.ClearAllImmediately();
+                i.Task.FollowToOffsetFromEntity(player, (player.ForwardVector * 2), -1, 10);
+            }
+        }
 
-        //    return Task.FromResult(0);
+        private void delete() 
+        {
+            API.SetNuiFocus(false, false);
+            foreach (Ped i in peds)
+            {
+                i.Delete();
+            }
+            peds = new Ped[0];
+        }
+        private void kill()
+        {
+            API.SetNuiFocus(false, false);
+            foreach (Ped i in peds)
+            {
+                i.Kill();
+            }
+            peds = new Ped[0];
+        }
+
+        private void cancel()
+        {
+            API.SetNuiFocus(false, false);
+        }
+
+        private void follow(int source, List<object> args, string raw)
+        {
+            API.SetNuiFocus(true, true);
+            if (peds.Length > 0) 
+            {
+                SendNuiMessage("{\"action\":\"menu\"}");
+            } 
+            else
+            {
+                SendNuiMessage("{\"action\":\"start\"}");
+            }
+            
+            //if ((bool)args.Any())
+            //{
+            //    if (peds.Length != 0)
+            //    {
+            //        foreach (Ped i in peds)
+            //        {
+            //            i.Delete();
+            //        }
+            //    }
+            //    byte cont = 4;
+            //    if (args.ElementAtOrDefault(1) != null)
+            //    {
+            //        if (byte.TryParse(args[1].ToString(), out _))
+            //        {
+            //            cont = Convert.ToByte(args[1]);
+            //            if (cont < 0 && cont > 40) //zmieniæ !!!!
+            //            {
+            //                cont = 4;
+            //            }
+            //        }
+            //    }
+            //}
+            //else
+            //{
+            //    foreach (Ped i in peds)
+            //    {
+            //        i.Delete();
+            //    }
+            //    peds = new Ped[0];
+            //}
+
+        }
+
+        //private async void follow_anim(int source, List<object> args, string raw)
+        //{
+        //    String ani1 = "", ani2 = "";
+        //    if (args.Count >= 2)
+        //    {
+        //        ani1 = args[0].ToString();
+        //        ani2 = args[1].ToString();
+        //    }
+        //    if (ani1 == "" && ani2 == "")
+        //    {
+        //        Ped player = Game.Player.Character;
+        //        foreach (Ped i in peds)
+        //        {
+        //            i.Task.ClearAllImmediately();
+        //            i.Task.FollowToOffsetFromEntity(player, (player.ForwardVector * 2), -1, 10);
+        //        }
+        //    }
+        //    while (!API.HasAnimDictLoaded(ani1))
+        //    {
+        //        API.RequestAnimDict(ani1);
+        //        await BaseScript.Delay(100);
+        //    }
+        //    AnimationFlags flags = AnimationFlags.Loop | AnimationFlags.CancelableWithMovement;
+        //    foreach (Ped i in peds)
+        //    {
+        //        i.Task.ClearAllImmediately();
+        //        i.Task.PlayAnimation(ani1, ani2, -1, -1, flags);
+        //    }
+
         //}
     }
 }
--- nui/follow.js
+++ nui/follow.js
@@ -1,6 +1,8 @@
 window.addEventListener("message", function (event) {   
   if(event.data.action == "start") {
-    this.document.body.style.display = "block";
+    $(".form-container").css({"display":"block"});
+  }else if(event.data.action == "menu") {
+    $(".form-container2").css({"display":"block"});
   }
 });
 
@@ -11,18 +13,62 @@ document.getElementById("submitButton").addEventListener("click", function(event
   let Cont = parseInt(document.getElementById("data2").value);
   if(isNaN(Cont)) {
     Cont = 4; 
+  } else {
+    if(Cont < 0 && Cont > 20) {
+      Cont = 4;
+    }
   }
   let armed = document.getElementById("check1").checked;
   let combat = document.getElementById("check2").checked;
   let weapon = document.getElementById("data3").value;
 
-  console.log(armed);
-  document.getElementsByTagName('body')[0].style.display = "none";
-  $.post("https://c_follow/spawn", JSON.stringify({
+  $(".form-container").css({"display":"none"});
+  $.post("https://c_follow/c_spawn", JSON.stringify({
       model: Model,
       cont: Cont,
       armed: armed,
       combat: combat,
-      weapon: weapon,
+      weapon: weapon
   }));
+});
+
+document.getElementById("submitButton2").addEventListener("click", function(event) {
+  event.preventDefault();
+  $(".form-container").css({"display":"none"});
+  $(".form-container2").css({"display":"none"});
+  $.post("https://c_follow/c_cancel", JSON.stringify({}));
+});
+
+document.getElementById("submitButton3").addEventListener("click", function(event) {
+  event.preventDefault();
+  
+  let dist = document.getElementById("data4").value; 
+  let anim = document.getElementById("data5").value; 
+
+  $(".form-container2").css({"display":"none"});
+  $.post("https://c_follow/c_anim", JSON.stringify({
+      dist: dist,
+      anim: anim
+  }));
+});
+
+document.getElementById("submitButton4").addEventListener("click", function(event) {
+  event.preventDefault();
+  
+  $(".form-container2").css({"display":"none"});
+  $.post("https://c_follow/c_delete", JSON.stringify({}));
+});
+
+document.getElementById("submitButton5").addEventListener("click", function(event) {
+  event.preventDefault();
+  
+  $(".form-container2").css({"display":"none"});
+  $.post("https://c_follow/c_kill", JSON.stringify({}));
+});
+
+document.getElementById("submitButton6").addEventListener("click", function(event) {
+  event.preventDefault();
+  
+  $(".form-container2").css({"display":"none"});
+  $.post("https://c_follow/c_follow", JSON.stringify({}));
 });
